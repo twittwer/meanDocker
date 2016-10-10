61141563b6a0a18addd40b23ab14166c7bc48cfd
Tobias Wittwer <t.wittwer95@gmail.com>
2016-10-10 23:47:45 +0200

    Implements Angular2 as Frontend-Framework

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
 
 var port = 8000;
 
@@ -9,11 +9,11 @@ app.get('/', function(req, res){
   res.send('Backend - It works!');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+//io.on('connection', function(socket){
+//  socket.on('chat message', function(msg){
+//    io.emit('chat message', msg);
+//  });
+//});
 
 http.listen(port, function(){
   console.log('Backend-App is listening on port ' + port);
