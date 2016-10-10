b20314f0fcdac4aad1c41987a4aef40b3c986163
Tobias Wittwer <t.wittwer95@gmail.com>
2016-10-10 10:40:21 +0200

    Initial Commit
    > creates simple node chat application
    > uses nodeJS, express, socketIO

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = 8000;
+
+app.get('/', function(req, res){
+  //res.sendFile(path.resolve(__dirname + '/../client/index.html'));
+  res.send('Backend - It  works!');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(port, function(){
+  console.log('Backend-App is listening on port ' + port);
+});
