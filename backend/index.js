f26f8bd8ab89cf9cf5dc74cc617cff586d2d7035
Tobias Wittwer <t.wittwer95@gmail.com>
2016-10-16 14:14:53 +0200

    Add MongoDB to Setup

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,21 @@ var path = require('path');
 var app = require('express')();
 var http = require('http').Server(app);
 //var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 var port = 8000;
+
+var dbURI = 'mongodb://mongodb/app';
+mongoose.connect(dbURI);
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
 
 app.get('/', function(req, res){
   res.send('Backend - It works!');
