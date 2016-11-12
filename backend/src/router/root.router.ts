5a4a6a7d9fd65d1620960a434635f877516e70be
Tobias Wittwer <t.wittwer95@gmail.com>
2016-11-12 22:03:43 +0100

    [CLEANUP] Cleanup sample code

--- backend/src/router/root.router.ts
+++ backend/src/router/root.router.ts
@@ -1,17 +1,12 @@
-import * as express from "express";
-import AbstractRouter from "./abstract-router";
-import PostRouter from "./post.router";
-import RootController from "../controller/root.controller";
-import { ControllerResult, sendControllerResult } from "../controller/controller-result.interface";
+import * as express from 'express';
+import AbstractRouter from './abstract-router';
 
 export default class RootRouter extends AbstractRouter {
-  configure(): void {
-    //noinspection TypeScriptValidateTypes
-    this._router.use('/posts', new PostRouter().getRouter());
+  configure (): void {
 
-    this._router.get('/', (req: express.Request, res: express.Response) => {
-      RootController.welcome()
-        .then((result: ControllerResult) => sendControllerResult(result, res));
-    });
+    // this._router.use( '/users', new PostRouter().getRouter() );
+
+    this._router.get( '/', ( req: express.Request, res: express.Response ) => {
+    } );
   }
 }
